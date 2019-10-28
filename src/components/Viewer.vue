17e14f47b7e80ab829fa966ba0bd164d0e7ed1dc
Evan Moon <bboydart91@gmail.com>
2019-10-28 22:46:19 +0900

    Viewer 컴포넌트 내 렌더러에 raycaster 추가

--- src/components/Viewer.vue
+++ src/components/Viewer.vue
@@ -3,13 +3,18 @@
 </template>
 
 <script>
+import * as THREE from 'three';
 import { mapState, mapMutations } from 'vuex';
 import {
   SET_RENDERER_SIZE,
   ADD_OBJECT,
   UPDATE_ASPECT_OF_MAIN_CAMERA,
   REMOVE_ALL_OBJECTS,
+  SET_SELECTED_OBJECT,
 } from '../stores/types';
+
+const raycaster = new THREE.Raycaster();
+const mousePosition = new THREE.Vector2();
 
 export default {
   name: 'Viewer',
@@ -27,11 +32,25 @@ export default {
       this.controls.update();
       this.renderer.render(this.scene, this.mainCamera);
     },
+    onClickObject (event) {
+      event.preventDefault();
+
+      const width = this.$el.clientWidth;
+      const height = this.$el.clientHeight;
+      mousePosition.x = (event.offsetX / width) * 2 - 1;
+      mousePosition.y = -(event.offsetY / height) * 2 + 1;
+      raycaster.setFromCamera(mousePosition, this.mainCamera);
+      const intersects = raycaster.intersectObjects(this.scene.children, true);
+      if (intersects.length) {
+        this.setSelectedObject(intersects[0].object);
+      }
+    },
     ...mapMutations({
       setRendererSize: SET_RENDERER_SIZE,
       addObject: ADD_OBJECT,
       removeAllObjects: REMOVE_ALL_OBJECTS,
       updateAspectOfMainCamera: UPDATE_ASPECT_OF_MAIN_CAMERA,
+      setSelectedObject: SET_SELECTED_OBJECT,
     }),
   },
   mounted () {
@@ -41,10 +60,13 @@ export default {
     const height = this.$el.clientHeight;
     this.updateAspectOfMainCamera(width / height);
     this.setRendererSize({ width, height });
-
     this.$el.appendChild(this.renderer.domElement);
 
+    this.renderer.domElement.addEventListener('mousedown', this.onClickObject, false);
     this.animate();
+  },
+  beforeMount () {
+    this.renderer.domElement.removeEventListener('mousedown', this.onClickObject);
   },
 };
 </script>
